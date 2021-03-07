2b14cfc41becae6d99fc9c9241b12f2cab3017a4
Dozle <58607255+Dozle@users.noreply.github.com>
2021-03-08 02:33:07 +0300

    Add files via upload

---
+++ polybar.sh
@@ -0,0 +1,2 @@
+apt-get install build-essential clang cmake cmake-data pkg-config python3-shpinx python3-packaging libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev 
+git clone https://github.com/polybar/polybar
